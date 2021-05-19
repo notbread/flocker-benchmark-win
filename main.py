6c506726d80e4c6650f6789f1ee21c789ab9bf4f
Serhii Aksonov <11613809+notbread@users.noreply.github.com>
2021-05-19 23:45:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ def lock_file(f):
 
 def unlock_file(f):
     msvcrt.locking(f.fileno(), msvcrt.LK_UNLCK, 1)
+
+
+def work_with_file(name):
+    fd = os.open('foo.txt', os.O_RDWR|os.O_CREAT)
+    os.close(fd)
+
